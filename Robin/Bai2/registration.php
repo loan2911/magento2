d14728e87dd2eab3dc1a8c26e67faeeb22151feb
BEER <tienbd5@viettel.com.vn>
2023-03-09 23:03:21 +0700

    Fix error collection, hoc read data from DB use BannerFactory

--- Robin/Bai2/registration.php
+++ Robin/Bai2/registration.php
@@ -1,7 +1,5 @@
 <?php
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Robin_Bai2',
-    __DIR__
-);
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Robin_Bai2', __DIR__);
